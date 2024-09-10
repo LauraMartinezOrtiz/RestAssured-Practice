6ccc03f6f7512831260385d502d11c4aaffd1ab8
Laura Martinez Ortiz <l.martinezortiz@globant.com>
2024-09-10 12:34:06 -0500

    Schemas added

--- src/main/java/com/automation/api/models/Resource.java
+++ src/main/java/com/automation/api/models/Resource.java
@@ -1,4 +1,21 @@
 package com.automation.api.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Resource {
+    private String id;
+    private String name;
+    private String trademark;
+    private int stock;
+    private double price;
+    private String description;
+    private String tags;
+    private boolean active;
 }
